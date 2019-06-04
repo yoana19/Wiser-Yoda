7f6287a688a87829fe37a8aa6dd5f592a0262954
Yoana Stankova <yoana19st@gmail.com>
2019-06-05 01:08:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from gpiozero import Button
+
+button=Button(3)
+button=Button(3)
+button=Button(3)
+button=Button(3)
+button=Button(3)
+button=Button(3)
+
+pygame.init()
+
+ = pygame.mixer.Sound('.wav')
+
+.play()
